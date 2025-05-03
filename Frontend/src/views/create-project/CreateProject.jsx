567a8b96dbc74a5f1dd7bfcbf88bca3d620122e7
sameer2210 <sameerkhan27560@gmail.com>
2025-05-03 11:52:33 +0530

    Responsive

--- Frontend/src/views/create-project/CreateProject.jsx
+++ Frontend/src/views/create-project/CreateProject.jsx
@@ -75,7 +75,7 @@ const CreateProject = () => {
         initial={{ opacity: 0, scale: 0.9 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{ duration: 0.5 }}
-        className="h-1/3 w-1/3 flex flex-col gap-8 justify-center items-center p-8 rounded-2xl bg-white/10 border border-white/20 shadow-xl backdrop-blur-md"
+        className="h-1/3 lg:w-1/3  flex flex-col gap-8 justify-center items-center p-8 rounded-2xl bg-white/10 border border-white/20 shadow-xl backdrop-blur-md"
       >
         <h2 className="text-4xl font-bold text-white text-center mb-6 ">
            Create a New Project
@@ -89,14 +89,14 @@ const CreateProject = () => {
             value={projectName}
             onChange={(e) => setProjectName(e.target.value)}
             required
-            className="w-full px-8 py-6 text-2xl rounded bg-white/20 text-white placeholder-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-50 backdrop-blur-sm"
+            className="w-full  px-8 py-6 text-2xl rounded bg-white/20 text-white placeholder-gray-300 focus:outline-none focus:ring-2 focus:ring-gray-50 backdrop-blur-sm"
           />
 
           <motion.button
             type="submit"
             whileHover={{ scale: 1.05 }}
             transition={{ type: 'spring', stiffness: 300 }}
-            className="w-1/2 py-3 text-lg font-bold bg-gradient-to-r border-1 border-black text-white to-emerald-400 text-black rounded-xl shadow-md hover:to-emerald-500 transition-all"
+            className="w-1/2 py-3 text-lg  font-bold bg-gradient-to-r border-1 border-black text-white to-emerald-400 text-black rounded-xl shadow-md hover:to-emerald-500 transition-all"
           >
             Create Project
           </motion.button>
--- Frontend/src/views/home/Home.jsx
+++ Frontend/src/views/home/Home.jsx
@@ -102,7 +102,7 @@ const Home = () => {
           </motion.div>
         ) : (
           <motion.div
-            className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-16 mt-5 ml-4 p-4"
+            className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  gap-16 mt-5 ml-4 p-4"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 0.7 }}
